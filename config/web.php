61ef6e8b2ff5276b01e19f737017c2d051a4cedb
Nikolay <devmikola@gmail.com>
2016-03-08 14:01:25 +0200

    Revert to timezone Kiev

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
-    'timeZone' => 'UTC',
+    'timeZone' => 'Europe/Kiev',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
